38042894b611798a188a0b42529c1da820bc63c0
biplob61 <bhbiplob61@gmail.com>
2021-02-03 00:47:41 +0600

    trueFalse

---
+++ trueFalse.js
@@ -0,0 +1,7 @@
+const name = "";
+if (name.length) {
+    console.log("conditon is true");//string a kisu thaklei true dhore newa hy 
+}
+else{
+    console.log("condition is false");//empty string a false dhore ney javaScript
+}
