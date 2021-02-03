920606353d13e0825ee3ce8f1d83cd723eed90af
biplob61 <bhbiplob61@gmail.com>
2021-02-03 15:48:58 +0600

    objects

---
+++ map-filter.js
@@ -0,0 +1,29 @@
+// const number = [2,3,4,5,6];
+// const output = [];
+// for (let i = 0; i < number.length; i++) {
+//     const element = number[i];
+//     const result = element * element;
+//     output.push(result);
+// }
+//console.log(output);
+
+
+// const square = number.map(function(number){
+//     return number*number;
+    
+// })
+// console.log(square);
+
+// const square = number => number*number;
+// console.log(square);
+
+//const result = number.map(x=>x*x);
+//const result = number.map(number=>number*number);
+//console.log(result);
+
+//filer
+//const bigger = number.filter(x=>x>5);
+// const bigger = number.find(x=>x>5);
+// console.log(bigger);
+
+
---
+++ objects.js
@@ -0,0 +1,14 @@
+const student= [
+    { name: "biplob", id: 21},
+     { name: "moni", id: 18}
+ ]
+ const names = student.map(s => s.name);
+ const ids = student.map(i =>i.id);
+ console.log(names);
+ console.log(ids);
+
+ const big = student.filter(s=>s.id>20);
+ console.log(big);
+
+ const bigger = student.find(s => s.id>20);
+ console.log(bigger);
