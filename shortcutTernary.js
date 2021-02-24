2993613b5b8569e319dfc15350ba4199acae4525
biplob61 <bhbiplob61@gmail.com>
2021-02-24 18:34:20 +0600

    ternary

---
+++ shortcutTernary.js
@@ -0,0 +1,22 @@
+const money = 150;
+ let food;
+if (money > 100) {
+    food = 'biriany';
+}
+else{
+    food = 'vorta';
+}
+
+
+//ternary 
+//condition ? true value : false value
+let food = money>100 ? 'biriany' : 'vorta';
+
+//same as function can be used 
+//when we use single function
+condition && function()
+
+// string to numbr
+const num = +"45";
+
+const numText = 45+""
